295c3b2868b80d1b89b84646c5570b04da07c7b2
miaowu <132597811+wdnmd710@users.noreply.github.com>
2025-03-13 20:11:53 +0800

    Update FileInfoDTO.java

--- src/main/java/com/project/template/dto/FileInfoDTO.java
+++ src/main/java/com/project/template/dto/FileInfoDTO.java
@@ -14,6 +14,7 @@ import java.nio.file.Paths;
 public class FileInfoDTO {
 
     private String server;
+    private int ceshi;
     private String key;
     private String bucket;
     private String alt;
